7fce4e2deb1e64612d7ec5cf094c6b01f00a55da
alamin-karno <amin15-1951@diu.edu.bd>
2023-05-10 14:44:44 +0600

    Shell screen and welcome screen added

--- lib/features/splash/presentation/pages/splash.page.dart
+++ lib/features/splash/presentation/pages/splash.page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:web_portfolio/helpers/utils.dart';
 import 'package:web_portfolio/styles/colors.dart';
 
+import '../../../welcome/presentation/pages/welcome.page.dart';
 import '../widgets/splash_loading.dart';
 
 class SplashPage extends StatelessWidget {
@@ -9,6 +12,10 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      GoRouter.of(Utils.mainNav.currentContext!).go(WelcomePage.route);
+    });
+
     return const Scaffold(
       backgroundColor: PersonalPortfolioColors.mainBlue,
       body: SplashLoading(
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,17 +1,34 @@
 import 'package:go_router/go_router.dart';
 
+import '../features/shell/presentation/pages/portfoliomain.page.dart';
 import '../features/splash/presentation/pages/splash.page.dart';
+import '../features/welcome/presentation/pages/welcome.page.dart';
 import '../helpers/utils.dart';
 
 class AppRoutes {
   static final router = GoRouter(
-      initialLocation: SplashPage.route,
-      navigatorKey: Utils.mainNav,
-      routes: [
-        GoRoute(
-          parentNavigatorKey: Utils.mainNav,
-          path: SplashPage.route,
-          builder: (context, state) => const SplashPage(),
-        ),
-      ]);
+    initialLocation: SplashPage.route,
+    navigatorKey: Utils.mainNav,
+    routes: [
+      GoRoute(
+        parentNavigatorKey: Utils.mainNav,
+        path: SplashPage.route,
+        builder: (context, state) => const SplashPage(),
+      ),
+      ShellRoute(
+        navigatorKey: Utils.tabNav,
+        builder: ((context, state, child) {
+          return PortfolioMainPage(child: child);
+        }),
+        routes: [
+          GoRoute(
+              parentNavigatorKey: Utils.tabNav,
+              path: WelcomePage.route,
+              pageBuilder: (context, state) {
+                return const NoTransitionPage(child: WelcomePage());
+              }),
+        ],
+      ),
+    ],
+  );
 }
